665d960ec52255de7951c6eb78a02718f2e7dc88
xiaomengjiang <jiang.xiaom@northeastern.edu>
2023-03-31 01:06:42 -0400

    A8 Node server

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-const express = require('express')
+import express from 'express'
+import cors from 'cors'
+import HelloController from "./controllers/hello-controller.js"
+import UserController from "./controllers/users/users-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js"
+
 const app = express()
-app.get('/hello', (req, res) => {res.send('Life is good!')})
-app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
-app.listen(4000)
+app.use(cors())
+// app.get('/hello', (req, res) => {res.send('Life is good!')})
+// app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+app.use(express.json())
+TuitsController(app)
+HelloController(app)
+UserController(app)
+app.listen(process.env.PORT || 4000)
