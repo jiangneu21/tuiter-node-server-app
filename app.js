b65555b115794abc017dcde7d965ad21d488e96f
xiaomengjiang <jiang.xiaom@northeastern.edu>
2023-04-05 02:13:13 -0400

    A9

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 import express from 'express'
+import mongoose from "mongoose";
 import cors from 'cors'
 import HelloController from "./controllers/hello-controller.js"
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js"
 
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(cors())
 // app.get('/hello', (req, res) => {res.send('Life is good!')})
--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,33 +1,31 @@
 import posts from "./tuits.js";
-let tuits = posts;
-const createTuit = (req, res) => {
+import * as tuitsDao from '../tuits/tuits-dao.js'
+
+const createTuit = async (req, res) => {
     const newTuit = req.body;
-    newTuit._id = (new Date()).getTime();
     newTuit.userName = 'NASA';
     newTuit.likes = 0;
     newTuit.liked = false;
     newTuit.dislike = 0;
     newTuit.time = 'now';
     newTuit.image = '../../tuiter/images/nasa.jpeg';
-    tuits.push(newTuit);
-    res.json(newTuit);
+    const insertedTuit = await tuitsDao.createTuit(newTuit)
+    res.json(insertedTuit);
 }
-const findTuits = (req, res) => {
+const findTuits = async (req, res) => {
+    const tuits = await tuitsDao.findTuits()
     res.json(tuits);
 }
-const updateTuit = (req, res) => {
+const updateTuit = async (req, res) => {
     const tuitdIdToUpdate = req.params.tid;
     const updates = req.body;
-    const tuitIndex = tuits.findIndex(
-        (t) => t._id === tuitdIdToUpdate)
-    tuits[tuitIndex] = {...tuits[tuitIndex], ...updates};
-    res.sendStatus(200);
+    const status = await tuitsDao.updateTuit(tuitdIdToUpdate, updates);
+    res.json(status);
 }
-const deleteTuit = (req, res) => {
+const deleteTuit = async (req, res) => {
     const tuitIdToDelete = req.params.tid;
-    tuits = tuits.filter((t) =>
-                             t._id !== tuitIdToDelete);
-    res.sendStatus(200);
+    const status = await tuitsDao.deleteTuit(tuitIdToDelete);
+    res.json(status);
 }
 
 export default (app) => {
@@ -36,3 +34,41 @@ export default (app) => {
     app.put('/api/tuits/:tid', updateTuit);
     app.delete('/api/tuits/:tid', deleteTuit);
 }
+// import posts from "./tuits.js";
+// let tuits = posts;
+// const createTuit = (req, res) => {
+//     const newTuit = req.body;
+//     newTuit._id = (new Date()).getTime();
+//     newTuit.userName = 'NASA';
+//     newTuit.likes = 0;
+//     newTuit.liked = false;
+//     newTuit.dislike = 0;
+//     newTuit.time = 'now';
+//     newTuit.image = '../../tuiter/images/nasa.jpeg';
+//     tuits.push(newTuit);
+//     res.json(newTuit);
+// }
+// const findTuits = (req, res) => {
+//     res.json(tuits);
+// }
+// const updateTuit = (req, res) => {
+//     const tuitdIdToUpdate = req.params.tid;
+//     const updates = req.body;
+//     const tuitIndex = tuits.findIndex(
+//         (t) => t._id === tuitdIdToUpdate)
+//     tuits[tuitIndex] = {...tuits[tuitIndex], ...updates};
+//     res.sendStatus(200);
+// }
+// const deleteTuit = (req, res) => {
+//     const tuitIdToDelete = req.params.tid;
+//     tuits = tuits.filter((t) =>
+//                              t._id !== tuitIdToDelete);
+//     res.sendStatus(200);
+// }
+//
+// export default (app) => {
+//     app.post('/api/tuits', createTuit);
+//     app.get('/api/tuits', findTuits);
+//     app.put('/api/tuits/:tid', updateTuit);
+//     app.delete('/api/tuits/:tid', deleteTuit);
+// }
