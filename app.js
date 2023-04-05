34a79a0b182cf361a3c2db2bc853c73bdabae9df
xiaomengjiang <jiang.xiaom@northeastern.edu>
2023-04-05 19:10:19 -0400

    render error

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ import HelloController from "./controllers/hello-controller.js"
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js"
 
-//env
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/tuiter'
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/tuiter';
 mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(cors())
