f3c7a2bd10d223c404d1ead7f9ba0b5772e5d361
xiaomengjiang <jiang.xiaom@northeastern.edu>
2023-04-09 13:19:53 -0400

    try to connect mongo with render

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ import TuitsController from "./controllers/tuits/tuits-controller.js"
 //const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/tuiter';
 const CONNECTION_STRING = 'mongodb+srv://x1aom3ng:mongodbpassword@cluster0.6qkpu53.mongodb.net/tuiter?retryWrites=true&w=majority';
 mongoose.connect(CONNECTION_STRING);
+//
 const app = express()
 app.use(cors())
 // app.get('/hello', (req, res) => {res.send('Life is good!')})
